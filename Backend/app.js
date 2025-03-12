5ea3a73c180873d4d64823d036451aebcc5179b2
Ssiyam0123 <ssiyam563@gmail.com>
2025-03-12 22:56:28 +0600

    authorization with barer token

--- Backend/app.js
+++ Backend/app.js
@@ -6,9 +6,11 @@ const app = express();
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 connectToDb();
+const cookieParser = require("cookie-parser");
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
